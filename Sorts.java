dd93c9e76b57a316ea95b7443dce45005ad885e6
OQ901 <103903453+OQ901@users.noreply.github.com>
2023-03-02 08:38:24 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,1 +1,10 @@
-
+public class Sorts{
+  public static ArrayList<Integer> sortInts(ArrayList<Integer> initSet){
+      for (int x = initSet.size(); x > 1; x --){
+        for (int y = 0; y < initSet.size() - 1; y ++){
+          if (initSet.get(y) > arrayList.get(y + 1){
+            int a = initSet.get(y);
+            int b = initSet.get(y + 1);
+            initSet.set(y, b);
+            initSet.set(y + 1, a);
+          
